e57c47e3c4836712380e2de355dca4e8b58b2a43
toporek44 <37250074+toporek44@users.noreply.github.com>
2020-01-15 21:28:25 +0100

    GUI added (not finished)

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 //Keyboard random colors generator
 const allKeys = [...document.querySelectorAll(".key")];
 const score = document.querySelector(".score");
-const rainbowBtn = document.querySelector("button");
-const lifes = [...document.querySelectorAll(".life")];
+const rainbowBtn = document.querySelector(".rainbow__switch");
+let lifes = [...document.querySelectorAll(".life")];
+const playBtn = document.querySelector(".play");
+const rankBtn = document.querySelector(".rank");
+const menuContainerDiv = document.querySelector(".menu__container");
+const containerDiv = document.querySelector(".container");
+const startTimerDiv = document.querySelector(".start__timer");
+const lifesDiv = document.querySelector(".lifes");
+const div = document.createElement("div");
 const Colors = {
   r: 0,
   g: 0,
@@ -10,11 +17,34 @@ const Colors = {
 };
 let scoreHandler = 0;
 let rainbowInterval;
+let startTimerInterval;
 let randomKeyIndex;
 let timer = 2000;
+let startTimer = 3;
 let rainbowFlag = false;
-let stopRandIndexFlag = true;
-// lifes.forEach(life => lifesContainer.appendChild(life));
+let stopRandIndexFlag = false;
+
+// div.classList.add("life");
+// lifesDiv.appendChild(div);
+
+const startGame = () => {
+  console.log("xd");
+  menuContainerDiv.style.transform = "translate(-50%,-200%)";
+  containerDiv.style.filter = "blur(0)";
+
+  startTimerInterval = setInterval(() => {
+    if (startTimer === 0) {
+      clearInterval(startTimerInterval);
+      startTimerDiv.style.opacity = 0;
+      setTimeout(() => {
+        stopRandIndexFlag = true;
+        randKeyIndexFnc();
+      }, 500);
+    }
+    startTimerDiv.textContent = startTimer--;
+  }, 1000);
+};
+
 const doRainbowInterval = () => {
   if (rainbowFlag) {
     Colors.r = Math.floor(Math.random() * 254);
@@ -54,28 +84,38 @@ rainbowBtn.addEventListener("click", () => {
 const checkTime = () => {
   if (allKeys[randomKeyIndex].classList.contains("key--active")) {
     setTimeout(() => {
-      if(allKeys[randomKeyIndex].classList.contains("key--active")){
-
-      lifes[lifes.length - 1].classList.remove("life");
-      lifes.splice(lifes.length - 1);
+      if (allKeys[randomKeyIndex].classList.contains("key--active")) {
+        lifes[lifes.length - 1].classList.remove("life");
+        lifes.splice(lifes.length - 1);
       }
-    }, timer-60);
+    }, timer - 60);
   }
 };
-const checkLifes = ()=>{
+const checkLifes = () => {
   if (lifes.length == 0) {
-    alert(`Your lifes have been finished. Your score is ${scoreHandler}!`)
-    console.log("LOSE");
-    stopRandIndexFlag = false
-    // timer=0;
+    menuContainerDiv.style.transform = "translate(-50%,-50%)";
+    containerDiv.style.filter = "blur(3px)";
+
+    playBtn.textContent = "PLAY AGAIN";
+    rankBtn.style.display = "none";
+    // menuContainerDiv.textContent = `
+    // You Lost!
+    // Score: ${scoreHandler}
+    // `;
+    stopRandIndexFlag = false;
+    startTimer = 3;
+    timer = 2000;
+    lifes = [...document.querySelectorAll(".life")];
+
+    lifes.forEach(life => life.classList.add("life"));
   }
-}
+};
 const randKeyIndexFnc = () => {
-  checkLifes()
-  
+  checkLifes();
+
   if (stopRandIndexFlag) {
     if (scoreHandler % 5 == 0 && scoreHandler != 0) {
-      timer -= 50;
+      timer -= 100;
       if (timer === 250) {
         timer = 250;
       }
@@ -91,13 +131,8 @@ const randKeyIndexFnc = () => {
     checkTime();
 
     setTimeout(randKeyIndexFnc, timer);
-    
-  
   }
-  
 };
-
-randKeyIndexFnc();
 
 const pickRandomKey = e => {
   console.log(`Kliknales: ${e.keyCode}`);
@@ -115,20 +150,19 @@ const pickRandomKey = e => {
       pressedCorrectKey.classList.remove("key--active");
     }
   } else {
-    checkLifes()
+    checkLifes();
 
     lifes[lifes.length - 1].classList.remove("life");
     lifes.splice(lifes.length - 1);
     // pressedCorrectKey.classList.add("key--correct");
     allKeys[randomKeyIndex].classList.remove("key--active");
 
-    // stopRandIndexFlag = false;
     const containsClassKeyWrong = allKeys.filter(key =>
       key.classList.contains("key--wrong")
     );
     if (containsClassKeyWrong.length === 0) {
       pressedWrongKey.classList.add("key--wrong");
-      setTimeout(()=>pressedWrongKey.classList.remove("key--wrong"),400)
+      setTimeout(() => pressedWrongKey.classList.remove("key--wrong"), 400);
     }
     // rainbowBtn.disabled = true;
     // rainbowStop();
@@ -140,3 +174,4 @@ const pickRandomKey = e => {
   );
 };
 window.addEventListener("keydown", pickRandomKey);
+playBtn.addEventListener("click", startGame);
